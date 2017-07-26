c1a31e214c1d092370a45b6b347da56a4cdf3fc9
DAVIS <md1691@att.com>
2017-07-25 21:11:25 -0700

    Begin tweaking index.html - 7/25/17

--- views/js/main.js
+++ views/js/main.js
@@ -503,9 +503,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.getElementsByClassName('mover');
-  var scroll_start = document.body.scrollTop / 1250;
+  var scrollStart = document.body.scrollTop / 1250;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin(scroll_start + (i % 5));
+    var phase = Math.sin(scrollStart + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
